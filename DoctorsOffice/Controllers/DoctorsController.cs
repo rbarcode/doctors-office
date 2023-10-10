9f0275a765837522d2c51dd6cd34f64fe8472566
Richard Barbour <richyb07@gmail.com>
2023-10-10 11:56:26 -0700

    Establish Doctor-side of many-to-many relationship.
    Co-authored-by: Jake Elsberry <jelsberry96@gmail.com>
    Co-authored-by: Onur Kaymak <onurkaymak@outlook.com>

--- DoctorsOffice/Controllers/DoctorsController.cs
+++ DoctorsOffice/Controllers/DoctorsController.cs
@@ -37,7 +37,10 @@ namespace DoctorsOffice.Controllers
 
     public ActionResult Details(int id)
     {
-      Doctor thisDoctor = _db.Doctors.FirstOrDefault(doctor => doctor.DoctorId == id);
+      Doctor thisDoctor = _db.Doctors
+              .Include(doctor => doctor.JoinEntities)
+              .ThenInclude(join => join.Patient)
+              .FirstOrDefault(doctor => doctor.DoctorId == id);
       return View(thisDoctor);
     }
 
@@ -69,5 +72,36 @@ namespace DoctorsOffice.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    public ActionResult AddPatient(int id)
+    {
+      Doctor thisDoctor = _db.Doctors.FirstOrDefault(doctors => doctors.DoctorId == id);
+      ViewBag.PatientId = new SelectList(_db.Patients, "PatientId", "Name");
+      return View(thisDoctor);
+    }
+    [HttpPost]
+    public ActionResult AddPatient(Doctor doctor, int patientId)
+    {
+      #nullable enable 
+      DoctorPatient? joinEntity = _db.DoctorPatients.FirstOrDefault(join => (join.PatientId == patientId && join.DoctorId == doctor.DoctorId ));
+      #nullable disable
+      if (joinEntity == null && patientId != 0)
+      {
+        _db.DoctorPatients.Add(new DoctorPatient() { PatientId = patientId, DoctorId = doctor.DoctorId });
+        _db.SaveChanges();
+      } 
+      return RedirectToAction("Details", new { id = doctor.DoctorId});
+    }
+
+    [HttpPost]
+
+    public ActionResult DeleteJoin(int joinId)
+    {
+      DoctorPatient joinEntry = _db.DoctorPatients.FirstOrDefault(join => join.DoctorPatientId == joinId);
+      _db.DoctorPatients.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");   
+    }
+
   }
 }
